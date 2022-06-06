ab89f7f963b8ee48c764d6cdbeaa9a90d91a0297
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-06-06 17:54:40 +0000

    Added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Hello."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Incorrect Password"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Incorrect Email"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
+
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
   resources "places"
   resources "posts"
   resources "sessions"
