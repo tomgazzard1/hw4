72d51f3583a9495c44443e6bc682c9226b8fa72e
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-06-06 19:23:44 +0000

    Edited navbar to include CSS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "Hello."
-        redirect_to "/posts"
+        redirect_to "/places"
       else
         flash["notice"] = "Incorrect Password"
         redirect_to "/login"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
     @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
