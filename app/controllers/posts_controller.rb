45d829ad95f45e778e9c5e11bd9e758e31cc0159
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-06-07 19:37:54 +0000

    Updates to posts form and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,8 @@ class PostsController < ApplicationController
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
